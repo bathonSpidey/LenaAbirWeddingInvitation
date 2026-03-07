de5ec4b3407d6a0ff309c8dfcd5f93ad6fd67c70
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2026-03-08 00:31:41 +0100

    all files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import EnvelopeIntro from "./components/EnvelopeIntro"
+
+function App() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-[#faf7f2]">
+      <EnvelopeIntro />
+    </div>
+  )
+}
+
+export default App
